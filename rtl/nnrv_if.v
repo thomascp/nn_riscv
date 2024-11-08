b030e856c47787b146439d91eb425f3ca15cd2e6
Peng Cheng <thomas.pcheng@gmail.com>
2024-11-08 15:45:35 +0100

    2024110801: refactor IF PC module, much clear now

--- rtl/nnrv_if.v
+++ rtl/nnrv_if.v
@@ -37,7 +37,7 @@ reg [INSTR_WIDTH-1:0] instr = {INSTR_WIDTH{1'b0}};
 
 reg [XLEN-1:0] cur_pc = {XLEN{1'b0}};
 
-assign o_ram_rd_addr = (i_id_hazard_stall) ? cur_pc : pc;
+assign o_ram_rd_addr = (i_id_jmp_stall) ? i_id_jmp_pc : pc;
 assign o_id_instr = instr;
 assign o_id_cur_pc = cur_pc;
 
@@ -49,34 +49,44 @@ end
 always @ (posedge i_clk or posedge i_rst) begin
     if (i_rst) begin
         pc <= {XLEN{1'b0}};
-    end else if (i_id_jmp_stall) begin
-        pc <= i_id_jmp_pc;
-    end else if (i_id_hazard_stall) begin
-        pc <= pc;
-    end else begin
-        pc <= pc + 4;
-    end
-end
-
-always @ (posedge i_clk or posedge i_rst) begin
-    if (i_rst) begin
-        instr <= {XLEN{1'b0}};
         cur_pc <= {XLEN{1'b0}};
-    end else if (i_id_jmp_stall || !i_ram_rd_ready) begin
         instr <= `OP_INSTR_NOP;
-        cur_pc <= cur_pc;
-    end else if (i_mem_ram_stall) begin
-        /* keep all as it is */
-    end else if (i_id_hazard_stall) begin
-        instr <= (cur_pc[2:0] == 3'b000) ?
+    end else if (i_id_jmp_stall) begin
+        /* 
+            when jmp_stall,
+            assign i_id_jmp_pc to o_ram_rd_addr, so can get instr when next posedge,
+            so cur_pc is the i_id_jmp_pc, pc is i_id_jmp_pc + 4.
+        */
+        pc <= i_id_jmp_pc + 4;
+        cur_pc <= i_id_jmp_pc;
+        instr <= (i_id_jmp_pc[2:0] == 3'b000) ?
             i_ram_rd_data[31 : 0] :
             i_ram_rd_data[63 : 32];
+    end else if (!i_ram_rd_ready) begin
+        /* 
+            when instr ram is not ready,
+            don't change pc, cur_pc, wait for ready signal,
+            but send NOP to the pipeline.
+        */
+        pc <= pc;
         cur_pc <= cur_pc;
+        instr <= `OP_INSTR_NOP;
+    end else if (i_id_hazard_stall || i_mem_ram_stall) begin
+        /* 
+            when stall,
+            don't change pc, cur_pc,
+            don't change instr, hazard stall use instr to produce hazard signal.
+        */
+        pc <= pc;
+        cur_pc <= cur_pc;
+        instr <= instr;
     end else begin
+        /* normal +4 next pc */
+        pc <= pc + 4;
+        cur_pc <= pc;
         instr <= (pc[2:0] == 3'b000) ?
             i_ram_rd_data[31 : 0] :
             i_ram_rd_data[63 : 32];
-        cur_pc <= pc;
     end
 end
 
