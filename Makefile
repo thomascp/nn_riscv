5f3ac7bf5b664c214d0fb58c2edadd43e8389fe4
Peng Cheng <thomas.pcheng@gmail.com>
2024-11-20 22:28:44 +0100

    2024112002: code clean

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@
 RTL=./rtl
 BOARD=./board
 
+prep:
+	sudo apt install iverilog gcc-riscv64-unknown-elf gtkwave
+
 compile:
 	iverilog -I$(RTL)/ -o nnrv $(RTL)/nnrv_mem.v $(RTL)/nnrv_wb.v $(RTL)/nnrv_exec.v $(RTL)/nnrv_reg.v $(RTL)/nnrv_id.v $(RTL)/nnrv_ram.v $(RTL)/nnrv_if.v $(RTL)/nnrv_top.v $(RTL)/nnrv_tb.v
 
--- rtl/nnrv_mem.v
+++ rtl/nnrv_mem.v
@@ -53,7 +53,6 @@ reg rd_en = 1'b0;
 reg [4:0] rd = 5'b0;
 reg rd_ready = 1'b0;
 reg [XLEN-1:0] rd_reg = {XLEN{1'b0}};
-reg ram_stall = 1'b0;
 wire ram_not_ready;
 
 assign o_wb_rd_en = rd_en;
@@ -74,23 +73,19 @@ assign o_id_rd_ready = rd_ready;
 assign o_id_rd = rd;
 assign o_id_rd_reg = rd_reg;
 
-assign ram_not_ready = ((i_exec_ram_rd_en && !i_ram_rd_ready) ||
-                        (i_exec_ram_wr_en && !i_ram_wr_ready));
-assign o_ram_stall = ram_stall;
+assign o_ram_stall = ((i_exec_ram_rd_en && !i_ram_rd_ready) ||
+                      (i_exec_ram_wr_en && !i_ram_wr_ready));
 
 always @ (posedge i_clk or posedge i_rst) begin
     if (i_rst) begin
         rd_en <= 1'b0;
         rd_ready <= 1'b0;
-        ram_stall <= 1'b0;
-    end else if (ram_not_ready) begin
+    end else if (o_ram_stall) begin
         rd_en <= 1'b0;
         rd_ready <= 1'b0;
-        ram_stall <= 1'b1;
     end else begin
         rd_en <= i_exec_rd_en;
         rd <= i_exec_rd;
-        ram_stall <= 1'b0;
         if (i_exec_ram_rd_en) begin
             case(i_exec_ram_mask)
                 8'b00000001:
